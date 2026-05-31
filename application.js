2a35b3b4a77ddab0103d2a22fe6e29ca2d8f441a
swatikadam15 <swatikadam169@gmail.com>
2026-05-31 16:50:44 +0000

    added localhost in code

--- application.js
+++ application.js
@@ -28,6 +28,6 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running at http://0.0.0.0:${PORT}/`);
+    console.log(`Server running at http://localhost:${PORT}/`);
     console.log(`Hostname: ${hostname}, Pod IP: ${podIP}`);
 });
